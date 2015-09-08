001993f22fa29758855c174e40791b89c67552bf
Ryan French <ryan@ryanfrench.co>
2015-09-08 16:23:23 +0100

    Added rake autocompletion

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -16,3 +16,6 @@ apt-get -y update >/dev/null
 
 # Install dependencies
 apt-get -y install facter linux-headers-$(uname -r) build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev curl unzip >/dev/null
+
+# Install bash completion for rake tasks
+sudo wget -O /etc/bash_completion.d/rake https://raw.githubusercontent.com/ai/rake-completion/master/rake
