0241a0873e864eb69b58a156b5828a362b37bf23
Ryan French <ryan@ryanfrench.co>
2015-11-13 00:06:31 +0000

    Install dependencies required for vmware/virtualbox tools

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -4,5 +4,11 @@
 
 export DEBIAN_FRONTEND="noninteractive"
 
+# Update the box
+apt-get -y update >/dev/null
+
+# Install dependencies
+apt-get -y install facter linux-headers-$(uname -r) build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev curl unzip >/dev/null
+
 # Install bash completion for rake tasks
 sudo wget -O /etc/bash_completion.d/rake https://raw.githubusercontent.com/ai/rake-completion/master/rake
