7b17eaae19b5ab8dcb46b36ff8e509bc5e8bd7b3
Ryan French <ryan@ryanfrench.co>
2015-11-15 20:21:41 +0000

    Install open-vm-tools

--- scripts/vmware.sh
+++ scripts/vmware.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-# Mount the disk image
-mkdir -p /mnt/vmware
-mount -o loop /home/vagrant/linux.iso /mnt/vmware
-
-# Install the drivers
-cd /tmp
-tar xzf /mnt/vmware/VMwareTools-*.tar.gz
-
-/tmp/vmware-tools-distrib/vmware-install.pl -d
-
-# Cleanup
-umount /mnt/vmware
-rm -fr /tmp/vmware-tools-distrib /home/vagrant/linux.iso
+# Install the VMWare Open Tools
+yum install -y open-vm-tools
