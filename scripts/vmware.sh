4a3ce12eaab6027cc4090e5ec049995e57d2e2e6
Ryan French <ryan@ryanfrench.co>
2015-11-16 10:02:31 +0000

    Install VMWare tools to get hgfs driver on top of open-vm-tools

--- scripts/vmware.sh
+++ scripts/vmware.sh
@@ -2,3 +2,17 @@
 
 # Install the VMWare Open Tools
 apt-get install -y open-vm-tools
+
+# Mount the disk image
+mkdir -p /mnt/vmware
+mount -o loop /home/vagrant/linux.iso /mnt/vmware
+
+# Install the drivers
+cd /tmp
+tar xzf /mnt/vmware/VMwareTools-*.tar.gz
+
+/tmp/vmware-tools-distrib/vmware-install.pl -d
+
+# Cleanup
+umount /mnt/vmware
+rm -fr /tmp/vmware-tools-distrib /home/vagrant/linux.iso
