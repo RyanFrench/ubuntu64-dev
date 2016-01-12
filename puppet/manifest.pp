8de12eab2f36fae4755ebcdb7165b2e452452dce
Ryan French <ryan@ryanfrench.co>
2016-01-12 16:25:41 +0000

    Add ruby 2.2.2 to the base image as well as 2.3.0

--- puppet/manifest.pp
+++ puppet/manifest.pp
@@ -2,6 +2,7 @@ class { 'rbenv': }
 
 rbenv::plugin { 'sstephenson/ruby-build': }
 rbenv::build { '2.3.0': global => true }
+rbenv::build { '2.2.2': global => false }
 rbenv::plugin { 'rbenv/rbenv-gem-rehash': }
 
 class { 'nvm':
