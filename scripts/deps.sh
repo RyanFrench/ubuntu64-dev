60b1d8e6d9785800f9c00b129e58cee5516b21bb
Ryan French <ryan@ryanfrench.co>
2016-02-03 09:35:24 +0000

    Install VIM into the image

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -8,7 +8,7 @@ export DEBIAN_FRONTEND="noninteractive"
 apt-get -y update >/dev/null
 
 # Install dependencies
-apt-get -y install linux-headers-$(uname -r) build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev curl unzip >/dev/null
+apt-get -y install vim linux-headers-$(uname -r) build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev curl unzip >/dev/null
 
 # Install bash completion for rake tasks
 sudo wget -O /etc/bash_completion.d/rake https://raw.githubusercontent.com/ai/rake-completion/master/rake
